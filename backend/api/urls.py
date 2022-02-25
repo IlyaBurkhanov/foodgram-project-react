11c7d5b5c46048b68055bd1d49dcf67577973b74
iabur <ia@ia.ru>
2022-02-26 00:14:06 +0300

    first

---
+++ backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+# from .views import CreateUserViewSet
+
+app_name = 'api'
+
+# router = routers.DefaultRouter()
+# router.register('users/', CreateUserViewSet, basename='create_users')
+
+urlpatterns = [
+    #path('users/')
+]
