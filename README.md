5e5efd06e4061b90c087c21cb6e1496e3e7968b7
iabur <ia@ia.ru>
2022-03-13 18:48:35 +0300

    first 5

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # praktikum_new_diplom
+Пока готова только первая часть - бэкенд. Без докеров и связи с фронтом. Позже добью.
+
+Бэкенд в папке backend
+
+## Шаги
+0. pip install -r requirements.txt
+
+1. python manage.py makemigrations
+
+2. python manage.py migrate
+
+3. python manage.py createsuperuser   (для админки)
+
+4. python manage.py load_ingredients --path ingredients.csv   (загрузка ингредиетов)
+
+
+Дальше можно тестировать
---
+++ backend/api/management/commands/load_ingredients.py
@@ -0,0 +1,20 @@
+import csv
+
+from django.core.management import BaseCommand
+from api.models import Ingredients
+
+
+class Command(BaseCommand):
+    help = 'Load Ingredients from csv'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str)
+
+    def handle(self, *args, **options):
+        path = options['path']
+        with open(path, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                Ingredients.objects.create(
+                    name=row[0],
+                    measurement_unit=row[1])
